4940ce838ce51d08d33daf3b644773edd0cf4937
giancoda93 <giancoda93@gmail.com>
2024-12-20 17:30:38 +0100

    Impostata la griglia per la data

--- src/components/form-components/DatePickerDesktop.jsx
+++ src/components/form-components/DatePickerDesktop.jsx
@@ -1,11 +1,10 @@
 import { eachDayOfInterval, format } from 'date-fns'
+import { useEffect, useState } from 'react'
 
 // Styles
 import "../../styles/react-components/DatePickerDesktop.css"
 
 // Costanti
-const today = new Date()
-const monthBeginning = new Date(today.getFullYear(), today.getMonth(), 1)
 const dayName = [
   [0, "Domenica"],
   [1, "Lunedì"],
@@ -15,13 +14,14 @@ const dayName = [
   [5, "Venerdì"],
   [6, "Sabato"],
 ]
+const today = dayToObject(new Date())
 
 // Funzioni
-function generateCalendar(year, month) {
+function generateMonth(year, month) {
   // genera un calendario, sotto forma di array, di due anni a partire dal mese corrente.
   // ogni elemento dell'array è un oggetto contenente anno, mese, giorno, e nome del giorno
-  const start = new Date(year, month, 1)
-  const end = new Date(year, month + 23, 0)
+  const start = new Date(year, month - 1, 1)
+  const end = new Date(year, month, 0)
 
   return eachDayOfInterval({ start, end }).map(date => dayToObject(date))
 }
@@ -32,25 +32,60 @@ function dayToObject(date) {
     year: date.getFullYear(),
     month: date.getMonth() + 1, // Mesi indicizzati da 0
     day: date.getDate(),
-    weekDay: dayName[date.getDay()][1],
+    weekDay: {
+      name: dayName[date.getDay()][1],
+      num: date.getDay(),
+    }
   }
 }
 
-// getDay()   restituisce il numero del giorno della settimane (es. Venerdì = 5)
-// getDate()  restituisce il numero del giorno del mese
-// getMonth() restituisce il numero del mese, partendo da 0 (es. Dicembre = 11)
+function getMonthGrid(month) {
+  // restituisce un array di 42 elementi posizionando i giorni del mese
+  // in base al giorno della settimana
+  const monthGrid = [...month]
+  
+  const firstDay = month[0].weekDay.num
+  const lastDay = month[month.length - 1].weekDay.num
+  const numDaysBefore = firstDay == 0 ? 6 : (firstDay - 1)
+  const numDaysAfter = lastDay == 0 ? 0 : (7 - lastDay)
+
+  for(let i = 0; i < numDaysBefore; i++) monthGrid.unshift("")
+  for(let i = 0; i < numDaysAfter; i++) monthGrid.push("")
+
+  return monthGrid
+}
+
+
 
 export default function DatePickerDesktop({ fieldId, checkInDate, checkOutDate, setCheckInDate, setCheckOutDate }) {
+  const [firstMonth, setFirstMonth] = useState({m: today.month, y: today.year})
+  const [secondMonth, setSecondMonth] = useState({m: today.month + 1, y: today.year})
+  const [firstMonthCalendar, setFirstMonthCalendar] = useState(generateMonth(firstMonth.y, firstMonth.m))
+  const [secondMonthCalendar, setSecondMonthCalendar] = useState(generateMonth(secondMonth.y, secondMonth.m))
+
+  useEffect(() => {
+    const show = getMonthGrid(firstMonthCalendar)    
+    console.log(show)
+  }, [firstMonthCalendar])
 
   return (
     <div className="date-picker">
       <h2>Scegli la data di {fieldId == "checkInDate" ? "Check-in" : "Check-out"}</h2>
       <div className="calendar">
-        <table>
-          <tbody>
-            
-          </tbody>
-        </table>
+        <div id='first-month' className='month'>
+          {getMonthGrid(firstMonthCalendar).map((d, idx) => (
+            <div key={idx} className="day">
+              {d ? d.day : ""}
+            </div>
+          ))}
+        </div>
+        <div id='second-month' className='month'>
+          {getMonthGrid(secondMonthCalendar).map((d, idx) => (
+            <div key={idx} className="day">
+              {d ? d.day : ""}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
