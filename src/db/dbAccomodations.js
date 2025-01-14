47e886e8f2471b599dcefcb484e00b11e48ffa92
giancoda93 <giancoda93@gmail.com>
2025-01-14 10:30:20 +0100

    Impostato il routing dinamico

--- src/db/dbAccomodations.js
+++ src/db/dbAccomodations.js
@@ -5,10 +5,16 @@ import { collection, query, where, getDocs } from "firebase/firestore"
 const accomodationsRef = collection(db, "accomodations")
 
 // estrae tutti gli alloggi in base alla query inserita
-export async function getAccomodations(country) {
+export async function getAccomodations(objQuery) {
   let accomodationsArray = []
+  let q = accomodationsRef
 
-  const q = query(accomodationsRef, where("country", "==", country))
+  // definisco il tipo di query
+  if (objQuery) {
+    if (objQuery.type == "country") {
+      q = query(accomodationsRef, where("country", "==", objQuery.value))
+    } 
+  }
 
   // snapshot per la query inserita
   const querySnapshot = await getDocs(q)
@@ -24,13 +30,26 @@ export async function getAccomodations(country) {
   return accomodationsArray
 }
 
+// estrae tutti gli alloggi
+export async function getAllAccomodations() {
+  let response = null
+
+  try {
+    response = await getAccomodations()
+  } catch (err) {
+    console.error("Errore nel caricamento dei document", err)
+    response = "Not found"
+  }
+
+  return response
+}
 
 // estrae gli alloggi disponibili in base al paese
 export async function getAccomodationsByCountry(country) {
   let response = null
 
   try {
-    response = await getAccomodations(country)
+    response = await getAccomodations({ type: "country", value: country})
   } catch (err) {
     console.error("Errore nel caricamento dei documenti", err)
     response = "Not found"
