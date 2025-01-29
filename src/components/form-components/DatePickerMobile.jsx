28819f0e19828c8b06f43fb7bfdeff72d5b93f23
giancoda93 <giancoda93@gmail.com>
2025-01-29 10:32:08 +0100

    Impostato selezione data mobile

--- src/components/form-components/DatePickerMobile.jsx
+++ src/components/form-components/DatePickerMobile.jsx
@@ -21,18 +21,53 @@ import { useStore } from "@nanostores/react"
 
 const today = dayToObject(new Date())
 
+// HANDLERS
+
+function handleClick (day, dates, setDates) {
+  let tempDates = [ ...dates ]
+
+  if (tempDates.length == 0) {
+    // se non sono state selezionate date, la aggiungo
+    tempDates.push(day)
+  } else if (tempDates.length == 1) {
+    // se è stata selezionata solo una data verifico se quella nuova è precedente a quella già presente
+    // se SI la aggiungo in posizione 0 dell'array, altrimenti in posizione 1
+    if (isDateObjBefore(day, tempDates[0])) {
+      tempDates.unshift(day)
+    } else {
+      tempDates.push(day)
+    }
+  } else if(tempDates.length == 2) {
+    // se sono già presenti due date: se il giorno selezionato è precedente a entrambi sostituisco il primo,
+    // se è successivo ad entrambi sostituisco il secondo, altrimenti svuoto l'array e inserisco il nuovo giorno
+    // come unico elemento
+    if (isDateObjBefore(day, tempDates[0])) {
+      tempDates.shift()
+      tempDates.unshift(day)
+    } else if (!isDateObjBefore(day, tempDates[1])) {
+      tempDates.pop()
+      tempDates.push(day)
+    } else {
+      tempDates = [day]
+    }
+  }
+
+  setDates([ ...tempDates ])
+}
+
 // --------------------------------------------------------------------
 // Funzione componente
 export default function DatePickerMobile({ title }) {
 
   const searchCriteria = useStore($searchCriteria)
 
-  const [firstMonthCalendar, setFirstMonthCalendar] = useState(generateMonth(today.year, today.month))
   const [showedMonths, setShowedMonths] = useState([ today.month, today.month + 1, today.month + 2, today.month + 3 ])
+        // TODO: andrà impostata la possibilità di aumentare i mesi visualizzati
+  const [selectedDates, setSelectedDates] = useState([])
 
   useEffect(() => {
-
-  }, [])
+    console.log(selectedDates)
+  }, [selectedDates])
 
   return (
     <>
@@ -56,7 +91,8 @@ export default function DatePickerMobile({ title }) {
                 {getMonthGrid(generateMonth(today.year, month)).map((d, idx) => (
                   <div className="day-mobile-container" key={idx}>
                     <div 
-                      className="day-mobile"
+                      className={`day-mobile ${isDateObjBefore(d, today) ? "day-mobile-before" : ""}`}
+                      onClick={() => handleClick(d, selectedDates, setSelectedDates)}
                     >
                       {d ? d.day : ""}
                     </div>
